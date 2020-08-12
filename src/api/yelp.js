8d39e5dc57c242463e376465470c1044a73bda45
Alejandro Gonzalez <33532595+wilson1234567891962@users.noreply.github.com>
2020-08-12 13:53:53 -0500

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
     Authorization:
-      'Bearer jCsdyK6MuIDd--F55VxqkDMA_m9SzQGzKHYzgt2BrnnGQyOH-3sJ7vrMWlzc0UQvcUTs7W9EQygmOCNiOcSZ3SE8vIcWQGSCpBRnnGS2xEQ4CfzOHCfzZkASA-YyX3Yx'
+      'Bearer '
   }
 });
